de8510012639645f476bb13ab7d7b6d79c191785
NexusNSK <122861273+NexusNSK@users.noreply.github.com>
2024-11-25 08:56:29 +0700

    Update SecurityConfig.java

--- src/main/java/ru/markov/application/security/SecurityConfig.java
+++ src/main/java/ru/markov/application/security/SecurityConfig.java
@@ -17,8 +17,8 @@ public class SecurityConfig extends VaadinWebSecurity {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests()
-                .requestMatchers("/images/*.png").permitAll();
+       // http.authorizeHttpRequests()
+            //    .requestMatchers("/images/*.png").permitAll();
         super.configure(http);
         setLoginView(http, LoginView.class);
     }
