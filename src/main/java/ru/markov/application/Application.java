484475ecf5165955705c2771e3549c0f8c1ff5a7
NexusNSK <john.nexus,jn@gmail.com>
2025-11-10 23:40:10 +0700

    offline режим

--- src/main/java/ru/markov/application/Application.java
+++ src/main/java/ru/markov/application/Application.java
@@ -2,6 +2,7 @@ package ru.markov.application;
 
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.component.page.Push;
+import com.vaadin.flow.server.PWA;
 import com.vaadin.flow.theme.Theme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,6 +12,12 @@ import ru.markov.application.views.BrigEdit;
 @SpringBootApplication
 @Push
 @Theme("brigapp")
+@PWA(name = "BrigApp",
+        shortName = "BrigApp",
+        description = "BrigApp mobile",
+        backgroundColor = "#ffffff",
+        themeColor = "#000000",
+        offlinePath = "offline.html")
 public class Application implements AppShellConfigurator {
     public static void main(String[] args) throws ClassNotFoundException {
         SpringApplication.run(Application.class, args);
